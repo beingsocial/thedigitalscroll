c92ad1e75745658d15585e29a92a4a807043a26f
beingsocial <67635048+beingsocial@users.noreply.github.com>
2020-11-03 23:21:16 +0530

    Update 2.0

---
+++ emailer.php
@@ -0,0 +1,24 @@
+<?php
+	$name = $_POST['name'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$email_from = 'thedigitalscrollofficial@gmail.com';
+	
+	$email_subject = "Enquiry from client";
+	
+	$email_body = "User name: $name.\n".
+					"User email: $visitor_email.\n".
+						"User message: $message.\n";
+					
+	$to = "pshehbaz94@gmail.com";
+	
+	$headers = "From: $email_from \r\n";
+	
+	$headers = "Reply-To: $visitor_email \r\n";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	
+	headers("location: index.html");
+	
+?>
